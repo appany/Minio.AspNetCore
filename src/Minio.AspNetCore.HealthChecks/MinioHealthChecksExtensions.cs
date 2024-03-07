e70f172de73728f582d9932fb2c8e6e819639545
Cvetomir Todorov <4437252+cvetomir-todorov@users.noreply.github.com>
2024-03-07 14:53:37 +0200

    Change health check to test if a single bucket exists (#72)
    * Add Rider-specific files to the .gitignore
    
    * Change health check to test if a single bucket with a customizable name exists ('health' by default) instead of listing all buckets

--- src/Minio.AspNetCore.HealthChecks/MinioHealthChecksExtensions.cs
+++ src/Minio.AspNetCore.HealthChecks/MinioHealthChecksExtensions.cs
@@ -5,10 +5,16 @@ namespace Minio.AspNetCore.HealthChecks
 {
   public static class MinioHealthChecksExtensions
   {
+    /// <summary>
+    /// Adds a health check for MinIO to the specified <see cref="builder"/>.
+    /// The health check tests if a bucket with the specified <see cref="bucket"/> name exists.
+    /// Whether the bucket actually exists doesn't matter - the health check is successful if the call to MinIO doesn't throw.
+    /// </summary>
     public static IHealthChecksBuilder AddMinio(
       this IHealthChecksBuilder builder,
       Func<IServiceProvider, MinioClient> factory,
       string name = "minio",
+      string bucket = "health",
       HealthStatus? failureStatus = null,
       IEnumerable<string>? tags = null,
       TimeSpan? timeout = null)
@@ -17,10 +23,18 @@ namespace Minio.AspNetCore.HealthChecks
       {
         throw new ArgumentNullException(nameof(builder));
       }
+      if (string.IsNullOrWhiteSpace(name))
+      {
+        throw new ArgumentException("Health check name should not be null or whitespace.", nameof(name));
+      }
+      if (string.IsNullOrWhiteSpace(bucket))
+      {
+        throw new ArgumentException("Health check bucket should not be null or whitespace.", nameof(bucket));
+      }
 
       return builder.Add(new HealthCheckRegistration(
         name,
-        sp => new MinioHealthCheck(factory.Invoke(sp)),
+        sp => new MinioHealthCheck(factory.Invoke(sp), bucket),
         failureStatus,
         tags,
         timeout));
